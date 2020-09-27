460f0dbaef2072a9209e97889ea48335bda94d37
davydow <davy.phayboun@gmail.com>
2020-09-27 13:34:36 -0600

    commit to feature branch

---
+++ src/com/automation/test/DemoTest.java
@@ -0,0 +1,15 @@
+package com.automation.test;
+
+public class DemoTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello!");
+		System.out.println("Hello World");
+		System.out.println("I want to make another test");
+		System.out.println("This is a test");
+	}
+
+}
+
+
---
+++ src/com/automation/test/FeaturesBranchFile.java
@@ -0,0 +1,9 @@
+package com.automation.test;
+
+public class FeaturesBranchFile {
+	
+	public static void main(String[] args) {
+		System.out.println("Feature branch file");
+	}
+
+}
