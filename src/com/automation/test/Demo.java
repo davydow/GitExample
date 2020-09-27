7cd6e535c5862c28311aabdf6c050c794d6726c8
davydow <davy.phayboun@gmail.com>
2020-09-27 11:01:22 -0600

    four commit

--- src/com/automation/test/Demo.java
+++ src/com/automation/test/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World Again!");
+		System.out.println("I want to make another corrections");
 	}
 
 }
