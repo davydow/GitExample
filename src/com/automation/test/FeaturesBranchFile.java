644507df43939bad8a3c259f32209f40ba157562
davydow <davy.phayboun@gmail.com>
2020-09-27 13:54:32 -0600

    commit to feature updated

--- src/com/automation/test/FeaturesBranchFile.java
+++ src/com/automation/test/FeaturesBranchFile.java
@@ -4,6 +4,9 @@ public class FeaturesBranchFile {
 	
 	public static void main(String[] args) {
 		System.out.println("Feature branch file");
+		System.out.println("Feature branch updated");
+		
+		
 	}
 
 }
